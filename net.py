288414f125e20762010ff0dae4d5e427a7efa80f
casey <cmatthew@uccs.edu>
2017-05-05 09:06:26 -0600

    bugged back prop

--- net.py
+++ net.py
@@ -4,8 +4,10 @@ import node
 #import sklearn.linear_model.LogisticRegression.predict_proba as softmax
 import numpy as np
 
-training_path = '/home/kmoney/Documents/neural_net/training.txt'
-testing_path = '/home/kmoney/Documents/neural_net/testing.txt'
+#training_path = '/home/kmoney/Documents/neural_net/training.txt'
+#testing_path = '/home/kmoney/Documents/neural_net/testing.txt'
+training_path = '/home/casey/PycharmProjects/neural_net/training.txt'
+testing_path = '/home/casey/PycharmProjects/neural_net/testing.txt'
 datas = data.readFromFile(training_path)
 trainingSet = data.readIntoClasses(datas)
 datas = data.readFromFile(testing_path)
@@ -26,9 +28,13 @@ inputLayer = []
 hiddenLayer = []
 outputLayer = []
 
-for x in range(0,63): inputLayer.append(node.neuron(learningRate,True,False))
-for x in range(0,63): hiddenLayer.append(node.neuron(learningRate,False, False))
-for x in range(0,7): outputLayer.append(node.neuron(learningRate,False,True))
+for x in range(0,63): inputLayer.append(node.neuron(learningRate,True,False,False))
+for x in range(0,63): hiddenLayer.append(node.neuron(learningRate,False, False,False))
+for x in range(0,7): outputLayer.append(node.neuron(learningRate,False,True,False))
+
+# add bias nodes to input and hidden layers
+inputLayer.append(node.neuron(learningRate,True,False,True))
+hiddenLayer.append(node.neuron(learningRate,False,False,True))
 
 network.append(inputLayer)
 network.append(hiddenLayer)
@@ -61,30 +67,41 @@ def softmax(x):
 
 def runDatum(network, datum):
 	outputs = []
-	print "input layer:"
+	#print "input layer:"
 	#propogate datum through input layer
-	for y in range(len(network[0])):
+	for y in range(0,len(network[0])-1):
+		#print y
 		network[0][y].activation(datum.data[y])
 		outputs.append(network[0][y].output)
-	raw_input("hidden layer")
+	#bias node
+	network[0][-1].activation(1)
+	outputs.append(network[0][-1].output)
+	#raw_input("hidden layer")
 	#propogate datum through the hidden layer
 	outputs1 = []
-	for x in range(0,len(network[1])):
+	for x in range(0,len(network[1])-1):
+		#print x
 		network[1][x].activation(outputs)
 		outputs1.append(network[1][x].output)
+	#bias node
+	network[1][-1].activation([1])
+	outputs1.append(network[1][-1].output)
 
-	raw_input("output layer")
+	#raw_input("output layer")
 	#propogate datum to output layer
 	finalOut = []
 	for x in range(0,len(network[2])):
 		network[2][x].activation(outputs1)
 		finalOut.append(network[2][x].output)
 
-	raw_input('next example')
+	mean = sum(finalOut) / 7
+	Rnge = max(finalOut) - min(finalOut)
+	for x in range(0,len(finalOut)):
+		finalOut[x] = (finalOut[x] - mean) / Rnge
 	#print finalOut
 	finalOut = softmax(finalOut)
-	print finalOut
-	#print sum(finalOut)
+	return finalOut
+	#print "Guess:", expected
 
 def backPropagate(network, trueOut):
 	# get errors in output layer
@@ -97,13 +114,17 @@ def backPropagate(network, trueOut):
 	#adjust weights for all nodes in network
 	for x in network[2]:
 		x.adjustWeight()
-		#reset input
-		x.input = 0
-		x.output = 0
+	for x in network[1]:
+		x.adjustWeight()
 
 for x in trainingSet:
-	runDatum(network, x)
-	#backPropagate(network, expected[x.label])
+	print x.label
+	exp = expected[x.label]
+	guess = runDatum(network, x)
+	for y in range(0,len(guess)):
+		print exp[y] - guess[y]
+	backPropagate(network, expected[x.label])
+	raw_input("next")
 
 
 
--- node.py
+++ node.py
@@ -2,7 +2,7 @@ import random
 from math import exp
 
 class neuron:
-	def __init__(self, Lrate, inputBool, outputBool):
+	def __init__(self, Lrate, inputBool, outputBool, biasBool):
 		self.weights = []
 		self.output = 0
 		self.error = 0
@@ -11,8 +11,9 @@ class neuron:
 		self.tDer = 0
 		self.inputNode = inputBool
 		self.outputNode = outputBool
+		self.bias = biasBool
 
-		for x in range(0,64):
+		for x in range(0,65):
 			if not inputBool: self.weights.append(random.random())
 			else: self.weights.append(1)
 
@@ -23,19 +24,22 @@ class neuron:
 			else:
 				self.input = 0
 				for x in range(0,len(input)):
-					print "input:", input[x], "weight", self.weights[x]
+					#print "input:", input[x], "weight", self.weights[x]
 					self.input += input[x] * self.weights[x]
-					self.input = self.input / 100
-			print 'total input', self.input
+					self.input = self.input
+			#print 'total input', self.input
 					#print "in", self.input
 
 			#apply to activation function
-			if self.outputNode:
-				pass
+			if self.inputNode:
+				self.output = self.input
+			if self.bias:
+				self.input = 1
+				self.output = 1
 			else:
 				self.output =  1.0 / (1.0 + exp(-self.input))
-			print "out", self.output
-			print '*******************************************'
+			#print "out", self.output
+			#print '*******************************************'
 
 			#calculate transfer derivative
 			self.tDer = self.output * (1.0 - self.output)
@@ -44,6 +48,7 @@ class neuron:
 	def calculateError(self, expected):
 			if self.outputNode:
 				self.error = (expected - self.output) * self.tDer
+				#print self.error
 
 	def adjustWeight(self):
 		for x in range(len(self.weights)):
